a739cadfddcc911dea04c168140095127d1b1b88
danielisgr8 <schubertmdaniel@gmail.com>
2019-12-23 21:58:45 -0600

    Client example code now imports `getLatency`.

--- example/client/src/index.js
+++ example/client/src/index.js
@@ -1,4 +1,16 @@
 import { getLatency } from "../../../src";
 
-// TODO: use getLatency
-console.log("test");
+const result = document.querySelector(".result");
+const form = document.querySelector("form");
+
+const updateLatency = async (sampleCount) => {
+  const avg = await getLatency(`http://${window.location.hostname}:1234`, sampleCount);
+  result.textContent = `Latency: ${avg} ms`;
+}
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+
+  const sampleCount = form.elements["samples"].value;
+  updateLatency(sampleCount);
+}
