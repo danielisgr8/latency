20081903430a8b02dbd7100cb3a92e4123821296
danielisgr8 <schubertmdaniel@gmail.com>
2020-01-23 15:51:14 -0600

    Example can now handle audio data.

--- example/client/src/index.js
+++ example/client/src/index.js
@@ -4,10 +4,18 @@ const latencyButton = document.getElementById("setLatency");
 const requestButton = document.getElementById("sendRequest");
 const respondButton = document.getElementById("respond");
 const result = document.getElementById("result");
+const audioInput = document.getElementsByName("audio")[0];
+const audio = document.getElementsByTagName("audio")[0];
 
 const baseUrl = `http://${window.location.hostname}:1234`;
 
 const syncModule = new SyncModule(`${baseUrl}/ping`);
+
+let audioResponse = false;
+audioInput.onchange = (e) => {
+  audioResponse = e.target.checked;
+  audio.controls = e.target.checked;
+}
 
 latencyButton.onclick = async () => {
   await syncModule.setLatency();
@@ -15,7 +23,12 @@ latencyButton.onclick = async () => {
 }
 
 requestButton.onclick = async () => {
-  result.textContent = await syncModule.request(`${baseUrl}/data`);
+  const response = await syncModule.request(`${baseUrl}/data`);
+  if(audioResponse) {
+    audio.src = `data:audio/ogg;base64,${response}`;
+  } else {
+    result.textContent = true;
+  }
 }
 
 respondButton.onclick = () => {
--- example/server/server.js
+++ example/server/server.js
@@ -1,6 +1,17 @@
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
 
+const args = process.argv.slice(2);
+const sendAudio = args.length > 0 && args[0] === "audio" ? true : false;
+
+let audio;
+if(sendAudio) {
+  console.log("Audio mode set");
+  audio = fs.readFileSync(`${__dirname}/../test.ogg`);
+}
+
+const port = 1234;
 const server = http.createServer();
 
 const responses = [];
@@ -9,7 +20,7 @@ const sendResponses = () => {
     res.end(JSON.stringify({
       time: 123,
       delay: 5000,
-      data: "hi"
+      data: sendAudio ? Buffer.from(audio).toString("base64") : "hi"
     }));
   });
 
@@ -34,4 +45,4 @@ server.on("request", (req, res) => {
   }
 });
 
-server.listen(1234);
+server.listen(port, () => console.log(`Server listening at port ${port}`));
